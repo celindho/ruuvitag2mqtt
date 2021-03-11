9f252daf74f291d9128b66aa0ed198be13d7fe2a
Christian Lindholm <christian.lindholm@iki.fi>
2021-03-12 01:14:11 +0200

    Initial implementation that posts over mqtt

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 const listener = require('./listener');
 const mqtt = require('mqtt');
 
-const entiresToAggregate = 3;
+var mqtt_client;
+
+const entiresToAggregate = 25;
 
 var valuemap = {};
 
@@ -15,34 +17,56 @@ function handleRuuviReading(tagid, data) {
   valuemap[tagid].push(data);
 
   if (valuemap[tagid].length >= entiresToAggregate) {
-    var history = valuemap[tagid];
-
-    var temperature = 0;
-    var humidity = 0;
-    var pressure = 0;
-    var battery = 0;
-
-    history.forEach((data, index) => {
-      temperature += data["temperature"];
-      humidity += data["humidity"];
-      pressure += data["pressure"];
-      battery += data["battery"];
-    });
-
-    data = {
-      "entry_count": history.length,
-      "humidity": Math.round(humidity / history.length * 10) / 10,
-      "temperature": Math.round(temperature / history.length * 100) / 100,
-      "pressure": Math.round(pressure / history.length * 100) / 100,
-      "battery": Math.round(battery / history.length)
-    };
-
-    valuemap[tagid] = [];
-
-    console.log(`Sending data on topic : "ruuvi/${tagid}/status": `, data);
-
+    sendDataForTag(tagid);
   }
 
+}
+
+function sendDataForTag(tagid) {
+  var history = valuemap[tagid];
+
+  var data = getAveragedDataForTag(tagid);
+
+  valuemap[tagid] = [];
+
+  console.log(`Sending data on topic : "ruuvi/${tagid}/status": `, data);
+
+  getMqttClient().publish(`ruuvi/${tagid}/status`, JSON.stringify(data));
+
+}
+
+function getAveragedDataForTag(tagid) {
+  var history = valuemap[tagid];
+
+  var temperature = 0;
+  var humidity = 0;
+  var pressure = 0;
+  var battery = 0;
+
+  history.forEach((data, index) => {
+    temperature += data["temperature"];
+    humidity += data["humidity"];
+    pressure += data["pressure"];
+    battery += data["battery"];
+  });
+
+  return {
+    "entry_count": history.length,
+    "humidity": Math.round(humidity / history.length * 10) / 10,
+    "temperature": Math.round(temperature / history.length * 100) / 100,
+    "pressure": Math.round(pressure / history.length * 100) / 100,
+    "battery": Math.round(battery / history.length)
+  };
+}
+
+function getMqttClient() {
+  if (!mqtt_client) {
+    mqtt_client = mqtt.connect({ host: "localhost", port: 1883, connectTimeout: 60 * 1000 });
+  }
+  if (!mqtt_client.connected) {
+    mqtt_client.reconnect();
+  }
+  return mqtt_client;
 }
 
 listener.start(handleRuuviReading);
