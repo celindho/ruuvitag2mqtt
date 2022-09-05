aff1bde630886f83b345922c50cb2d2b29273844
Christian Lindholm <christian.lindholm@iki.fi>
2022-09-06 00:47:11 +0300

    primitive version of mqtt listener

--- index.js
+++ index.js
@@ -28,6 +28,26 @@ if (settings.forwarding_mode) {
 }
 
 listener.start(handleRuuviReading, handleRuuviDiscovery);
+
+function mqttListener(topic, payload, packet) {
+  const discoveryRegEx = new RegExp(
+    `^${settings.mqtt_topic_prefix}/raw_forward/discovery/`
+  );
+  const dataRegEx = new RegExp(
+    `^${settings.mqtt_topic_prefix}/raw_forward/reading/`
+  );
+
+  if (dataRegEx.test(topic)) {
+    const mac = /[^\/]+$/gm.exec(topic)[0];
+    handleRuuviReading(mac, JSON.parse(payload.toString()));
+  } else if (discoveryRegEx.test(topic)) {
+    const mac = payload;
+    handleRuuviDiscovery(mac);
+  }
+}
+mqtt.registerListener(mqttListener);
+mqtt.subscribe(`${settings.mqtt_topic_prefix}/raw_forward/#`);
+
 mqtt.publish(
   `${settings.mqtt_topic_prefix}/broker/status`,
   "Ruuvi2MQTT started."
