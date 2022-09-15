76ece62ade7f843bdac3441ed43cd9a3313d90f3
Christian Lindholm <christian.lindholm@iki.fi>
2022-09-15 22:30:51 +0300

    removed obsolete code

--- forwarding_mode/devicemessagehandler.js
+++ forwarding_mode/devicemessagehandler.js
@@ -4,9 +4,6 @@ const { logger, settings } = require("../globals");
 
 const mqtt = require("../mqtt");
 
-var valuemap = {};
-var nextSendForMac = {};
-
 function handleRuuviReading(mac, data) {
   mqtt.publish(
     `${settings.mqtt_topic_prefix}/raw_forward/reading/${mac}`,
