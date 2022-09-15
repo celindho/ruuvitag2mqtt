17471bdd116e5ec46ff2be90b1a86d73745373e3
Christian Lindholm <christian.lindholm@iki.fi>
2022-09-15 22:29:57 +0300

    moved listener code to own file

--- index.js
+++ index.js
@@ -29,29 +29,12 @@ if (settings.forwarding_mode) {
 
   listener.start(handleRuuviReading, handleRuuviDiscovery);
 
+  require("./mqtt_listener").start(handleRuuviReading, handleRuuviDiscovery);
+
   setInterval(
     checkAndSendOveragedData,
     Math.ceil((settings.maxWaitSeconds * 1000) / 10)
   );
-
-  const discoveryRegEx = new RegExp(
-    `^${settings.mqtt_topic_prefix}/raw_forward/discovery/`
-  );
-  const dataRegEx = new RegExp(
-    `^${settings.mqtt_topic_prefix}/raw_forward/reading/`
-  );
-
-  function mqttListener(topic, payload, packet) {
-    if (dataRegEx.test(topic)) {
-      const mac = /[^\/]+$/gm.exec(topic)[0];
-      handleRuuviReading(mac, JSON.parse(payload.toString()));
-    } else if (discoveryRegEx.test(topic)) {
-      const mac = payload;
-      handleRuuviDiscovery(mac);
-    }
-  }
-  mqtt.registerListener(mqttListener);
-  mqtt.subscribe(`${settings.mqtt_topic_prefix}/raw_forward/#`);
 }
 
 mqtt.publish(
