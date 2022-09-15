c7e39ab081af3c50d0150b3cd07cff7efc9163f4
Christian Lindholm <christian.lindholm@iki.fi>
2022-09-15 22:06:18 +0300

    refactored code

--- index.js
+++ index.js
@@ -14,22 +14,25 @@ if (settings.useDummyData == "true") {
 logger.info("Starting the Ruuvi2MQTT converter.");
 logger.info("Settings: " + JSON.stringify(settings));
 
-var handleRuuviDiscovery, handleRuuviReading, checkAndSendOveragedData;
 
 if (settings.forwarding_mode) {
-  handleRuuviDiscovery = require("./forwarding_mode/discoveryhandler");
-  handleRuuviReading =
+  var handleRuuviDiscovery = require("./forwarding_mode/discoveryhandler");
+  var handleRuuviReading =
     require("./forwarding_mode/devicemessagehandler").handleRuuviReading;
+
+  listener.start(handleRuuviReading, handleRuuviDiscovery);
 } else {
-  handleRuuviDiscovery = require("./default_mode/discoveryhandler");
-  handleRuuviReading = require("./default_mode/devicemessagehandler").handleRuuviReading;
-  checkAndSendOveragedData =
-    require("./default_mode/devicemessagehandler").checkAndSendOveragedData;
-}
+  var handleRuuviDiscovery = require("./default_mode/discoveryhandler");
+  var {handleRuuviReading, checkAndSendOveragedData} =
+    require("./default_mode/devicemessagehandler");
 
-listener.start(handleRuuviReading, handleRuuviDiscovery);
+  listener.start(handleRuuviReading, handleRuuviDiscovery);
 
-function mqttListener(topic, payload, packet) {
+  setInterval(
+    checkAndSendOveragedData,
+    Math.ceil((settings.maxWaitSeconds * 1000) / 10)
+  );
+
   const discoveryRegEx = new RegExp(
     `^${settings.mqtt_topic_prefix}/raw_forward/discovery/`
   );
@@ -37,27 +40,22 @@ function mqttListener(topic, payload, packet) {
     `^${settings.mqtt_topic_prefix}/raw_forward/reading/`
   );
 
-  if (dataRegEx.test(topic)) {
-    const mac = /[^\/]+$/gm.exec(topic)[0];
-    handleRuuviReading(mac, JSON.parse(payload.toString()));
-  } else if (discoveryRegEx.test(topic)) {
-    const mac = payload;
-    handleRuuviDiscovery(mac);
+  function mqttListener(topic, payload, packet) {
+    if (dataRegEx.test(topic)) {
+      const mac = /[^\/]+$/gm.exec(topic)[0];
+      handleRuuviReading(mac, JSON.parse(payload.toString()));
+    } else if (discoveryRegEx.test(topic)) {
+      const mac = payload;
+      handleRuuviDiscovery(mac);
+    }
   }
+  mqtt.registerListener(mqttListener);
+  mqtt.subscribe(`${settings.mqtt_topic_prefix}/raw_forward/#`);
 }
-mqtt.registerListener(mqttListener);
-mqtt.subscribe(`${settings.mqtt_topic_prefix}/raw_forward/#`);
 
 mqtt.publish(
   `${settings.mqtt_topic_prefix}/broker/status`,
   "Ruuvi2MQTT started."
 );
-
-if (checkAndSendOveragedData) {
-  setInterval(
-    checkAndSendOveragedData,
-    Math.ceil((settings.maxWaitSeconds * 1000) / 10)
-  );
-}
 
 logger.info("Started the Ruuvi2MQTT converter.");
